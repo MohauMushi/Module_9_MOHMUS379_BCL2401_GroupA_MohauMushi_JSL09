f6c11371a34e54498c1a7e7b5e0bfa7063d02e18
Mohau Mushi <david1mushi1@gmail.com>
2024-04-11 20:07:27 +0200

    added: catch error statement

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
-    
+    .catch(err =>{
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI2NjE1OTR8&ixlib=rb-4.0.3&q=80&w=1080)`;
+        document.getElementById("author").textContent = `By: John Johnson`;
+    });
