c97a1a9071bce0ea604e9c9ef1c5310467403b56
Mohau Mushi <david1mushi1@gmail.com>
2024-04-12 14:56:45 +0200

    modified: js fetch & added try catch statement

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(response => response.json())
-    .then(data => {
-        
-        console.log(data.urls.regular)
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err =>{
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI2NjE1OTR8&ixlib=rb-4.0.3&q=80&w=1080)`;
-        document.getElementById("author").textContent = `By: John Johnson`;
-    });
+try{
+    const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await response.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch(err){
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI2NjE1OTR8&ixlib=rb-4.0.3&q=80&w=1080)`;
+    document.getElementById("author").textContent = `By: John Johnson`;
+}
 
 /*
 The below code it define an asynchronous function that fetches a random quote from an API,then
@@ -32,19 +29,13 @@ Once the JSON data is obtained, it is used to update the content of the HTML ele
 The name and icon of the cryptocurrency are added to the upper-left of the dashboard page, and the current price, 24-hour high, and 24-hour low prices are displayed.
 Any errors that occur during the process are caught and logged to the console.
 */
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(response => {
-        if(!response.ok){
+
+try {
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if(!response.ok){
         throw Error("Something went wrong")
         }
-    return response.json()
-    })
-    .then(data =>{
-    /**
-     * Add the name and icon of the cryptocurrency
-     * to the upper-left of the dashboard page
-     * Use `data.name` and `data.image.small` to access that info
-     */
+    const data = await response.json()
     document.getElementById("crypto-top").innerHTML = `
         <img src = ${data.image.small}/>
         <span> ${data.name}</span>
@@ -54,15 +45,17 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         <p> 📈  R ${data.market_data.high_24h.zar}</P>
         <p> 📉  R ${data.market_data.low_24h.zar}</P>
     `
-})
-.catch(error => console.log(error))
+} catch (err) {
+    console.log(err)
+}
+
 
 /*
 The below function(getCurrentTime) gets the current date and time using new Date(), then it updates the text content of the HTML element 
 with the ID "time" to display the current time in a short time format using toLocaleTimeString.
 The setInterval method is used to call the getCurrentTime function at regular intervals and the time is updated every second.
 */
-getCurrentTime = () => {
+const getCurrentTime = () =>{
     const date = new Date();
     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"});
 }
@@ -76,21 +69,22 @@ If the response is okay, the JSON data is accessed and used to construct the wea
 and city name are extracted from the data and dynamically inserted into the HTML content of an element with the ID "weather".
 Any errors that occur during the process are caught and logged to the console using the catch method. 
 */
-navigator.geolocation.getCurrentPosition(position => {
-fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-    .then(response => {
-        if(!response.ok){
+
+
+navigator.geolocation.getCurrentPosition(async position => {
+  try {
+    const response = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+    if(!response.ok){
         throw Error("Weather data not available")
-        }
-        return response.json()
-    })
-    .then(data => {
-        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        document.getElementById("weather").innerHTML = `
-        <img class="weather-img" src=${iconUrl} />
-        <p class="weather-temp">${Math.round(data.main.temp)}&#176c</p>
-        <p class="weather-city">${data.name}</p>
-        `
-    })
-    .catch(error => console.error(error))
-})
+    }
+    const data = await response.json()
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    document.getElementById("weather").innerHTML = `
+    <img class="weather-img" src=${iconUrl} />
+    <p class="weather-temp">${Math.round(data.main.temp)}&#176c</p>
+    <p class="weather-city">${data.name}</p>
+    `
+    } catch (error) {
+        console.error(error)
+    }
+});
